eddb5cea315c23caee0c5afddf1f1f44283a1f57
mert574 <mrty574@gmail.com>
2019-06-26 15:31:31 +0300

    Refactors: Better way of handling ws & http

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const Conversations = require('./ConversationManager.js');
+const uuid = require('uuid/v4');
 
 const agentId = "agent1";
 
 module.exports = function(router) {
-    router.get("/:accountId/init", (ctx) => {
+    router.get("/init", (ctx) => {
+        const accountId = uuid();
         const conversation = Conversations.initConversation([agentId, ctx.params.accountId]);
 
         ctx.status = 200;
